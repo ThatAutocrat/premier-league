61e2cfe9d503098ec494fd5eb255fd291f9bd867
Devashish Gogoi <hellonat3456@gmail.com>
2026-05-22 00:33:06 +0530

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -6,4 +6,5 @@ config :football_tracker, FootballTrackerWeb.Endpoint,
   server: true,
   http: [ip: {0, 0, 0, 0}, port: String.to_integer(System.get_env("PORT") || "4000")],
   url: [host: System.get_env("PHX_HOST") || "localhost", port: 443, scheme: "https"],
-  secret_key_base: System.get_env("SECRET_KEY_BASE") || raise("SECRET_KEY_BASE missing")
+  secret_key_base: System.get_env("SECRET_KEY_BASE") || raise("SECRET_KEY_BASE missing"),
+  check_origin: ["https://premier-league-fuan.onrender.com"]
